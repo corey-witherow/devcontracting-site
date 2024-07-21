184ec9045bcd3a06a450ab69128bbf4576c4f7e8
Corey Witherow <corey@cwitherow.com>
2024-07-21 13:22:21 -0400

    updated label

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -65,7 +65,7 @@ export default function Header() {
                     }}
                     className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
                   >
-                    Sign In
+                    Dev Sign In
                   </button>
                 )}
               </li>
